c2836666ce5796cd990891d15c4d19c77269520b
Michael Straus <mstraus@oxy.edu>
2021-01-12 09:11:44 -0800

    added all functions to create within the sudoku class

--- allthingssoduku/sudokuclass.py
+++ allthingssoduku/sudokuclass.py
@@ -7,17 +7,37 @@ class Sudoku:
         s = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         if s == d:
             goodLineFlag = True
-            # print("Good Line")
         else:
             goodLineFlag = False
-            # print("Bad Line")
         return goodLineFlag
 
-    def _inputLine(self):
+    def _getLine(self): #should be same as line input function
         pass
 
     def _fillLine(self, line):
         pass
+
+    def _checkGroups(self):
+        pass
+
+    def _getGroups(self): #same as find groups.py
+        pass
+
+    def checkSudoku(self):
+        pass
+
+    def getSudoku(self): #same as sudoku input
+        pass
+
+    def fillSudoku(self):
+        pass
+
+    def displaySudoku(self):
+        pass
+
+
+
+
 if __name__ == '__main__':
     sudoku = Sudoku()
     print(sudoku._checkLine([1, 2, 3, 4, 5, 6, 8, 7, 9]))
